9956df002939f07c3b68844809f29ea793a4426a
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-28 09:16:01 +0100

    Brew formula update for circumflex version 0.2.2

--- circumflex.rb
+++ circumflex.rb
@@ -2,16 +2,16 @@
 class Circumflex < Formula
   desc "circumflex is Hacker News on the command line"
   homepage ""
-  version "0.2.1"
+  version "0.2.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bensadeh/circumflex/releases/download/0.2.1/circumflex_0.2.1_Darwin_x86_64.tar.gz"
-    sha256 "bc9f2829a2caee6a5c4b56c3c2bddd93c608a1f6537741e6cea3818ff250915d"
+    url "https://github.com/bensadeh/circumflex/releases/download/0.2.2/circumflex_0.2.2_Darwin_x86_64.tar.gz"
+    sha256 "31ca3af85ec96a849fbcc67467e90bee3e9399104d727b08aa247014d70f7b19"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bensadeh/circumflex/releases/download/0.2.1/circumflex_0.2.1_Linux_x86_64.tar.gz"
-    sha256 "a83169acfe1192677fbe3e5416203368ad4a8ed80cbbbbfd0a812eb86ad82bda"
+    url "https://github.com/bensadeh/circumflex/releases/download/0.2.2/circumflex_0.2.2_Linux_x86_64.tar.gz"
+    sha256 "acd6269c1796b600f91d315568fa7cdbd97374b3ca7bb2725abca58ae70bb8ed"
   end
 
   def install
