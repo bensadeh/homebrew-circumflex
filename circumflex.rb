ddcc0f49fdb5c56c94ad5a72d7d13a4d1d1e1f44
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-07 20:48:41 +0200

    Brew formula update for circumflex version 0.16

--- circumflex.rb
+++ circumflex.rb
@@ -5,25 +5,25 @@
 class Circumflex < Formula
   desc "circumflex is Hacker News in your terminal"
   homepage "https://github.com/bensadeh/circumflex/"
-  version "0.15"
+  version "0.16"
   license "AGPL-3.0-only"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/bensadeh/circumflex/releases/download/0.15/circumflex_0.15_macOS_64-bit.tar.gz"
-    sha256 "3c85a070715a2856e242673a69986723190f3043c2ad9bb5e432a2a802340c79"
+    url "https://github.com/bensadeh/circumflex/releases/download/0.16/circumflex_0.16_macOS_64-bit.tar.gz"
+    sha256 "23d0eae9c1eddbc22de5574a9646f46bf777acf526139d07f8fd2ba067a4c780"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/bensadeh/circumflex/releases/download/0.15/circumflex_0.15_macOS_arm64.tar.gz"
-    sha256 "06c40887f3b760e8a3bf2d39f22d20b28a25391919f8a5ce16290dc2ff3ba98a"
+    url "https://github.com/bensadeh/circumflex/releases/download/0.16/circumflex_0.16_macOS_arm64.tar.gz"
+    sha256 "1650a0f2d2adc4dad542633179ef77e23c57584fc21cd322c4e8087772bbc85e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bensadeh/circumflex/releases/download/0.15/circumflex_0.15_Linux_64-bit.tar.gz"
-    sha256 "25e4641eb5a5a9becb39744335fe396f0b331e08e36d4c6d7de53c9df6a51ec2"
+    url "https://github.com/bensadeh/circumflex/releases/download/0.16/circumflex_0.16_Linux_64-bit.tar.gz"
+    sha256 "bbd11285fc332f41859f4e391b772dc9f7aceac324f18e10649220b3ff0b9112"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/bensadeh/circumflex/releases/download/0.15/circumflex_0.15_Linux_arm64.tar.gz"
-    sha256 "66416426aea5fb765008fcc54d84c1698b6967641bc9ed594e254059eb9653b1"
+    url "https://github.com/bensadeh/circumflex/releases/download/0.16/circumflex_0.16_Linux_arm64.tar.gz"
+    sha256 "e7b1d38ad0b703c04e8af2991fa700e6bf2284acc4fab3da7a6225a33e4b1344"
   end
 
   depends_on "less"
