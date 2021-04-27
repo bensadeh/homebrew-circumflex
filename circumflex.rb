90be8d1eb669cd396748f65ff912f56631c01664
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-27 20:19:06 +0200

    Brew formula update for circumflex version 1.1

--- circumflex.rb
+++ circumflex.rb
@@ -5,25 +5,25 @@
 class Circumflex < Formula
   desc "circumflex is Hacker News in your terminal"
   homepage "https://github.com/bensadeh/circumflex/"
-  version "1.0"
+  version "1.1"
   license "AGPL-3.0-only"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/bensadeh/circumflex/releases/download/1.0/circumflex_1.0_macOS_64-bit.tar.gz"
-    sha256 "83142e6bd0ebf040a085346b015eaccbe2b27aac895e1e55e495fa3fe2938af3"
+    url "https://github.com/bensadeh/circumflex/releases/download/1.1/circumflex_1.1_macOS_64-bit.tar.gz"
+    sha256 "619edbe70de893037247beaaae143d0b4b3ab89bcdf57fdd5be0f2a93cc7cc7d"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/bensadeh/circumflex/releases/download/1.0/circumflex_1.0_macOS_arm64.tar.gz"
-    sha256 "39282d2b26d5aa2dade87428ac7120e07b811e23c21d9ddb1ff62d15ed1c364a"
+    url "https://github.com/bensadeh/circumflex/releases/download/1.1/circumflex_1.1_macOS_arm64.tar.gz"
+    sha256 "cba5b0b2b5c47bd9c139bcdc2ead9b38489462e1b05d1b905670666e488b4496"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bensadeh/circumflex/releases/download/1.0/circumflex_1.0_Linux_64-bit.tar.gz"
-    sha256 "5719a0f23c872b3c12bc67251f5af68db32036e1066af57a132fb77601abd850"
+    url "https://github.com/bensadeh/circumflex/releases/download/1.1/circumflex_1.1_Linux_64-bit.tar.gz"
+    sha256 "56d79efc10cb961a837488c14ac2e69abcad76c17875aa9051f6a13487863582"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/bensadeh/circumflex/releases/download/1.0/circumflex_1.0_Linux_arm64.tar.gz"
-    sha256 "a63ffb507f598f99f01c3a5416880c106ded1be75a1f0f96dde69d5b1922f50d"
+    url "https://github.com/bensadeh/circumflex/releases/download/1.1/circumflex_1.1_Linux_arm64.tar.gz"
+    sha256 "3ee8f20ee0b87017da364250ebc95af394d275c30a246fce1d1e5da98d277022"
   end
 
   depends_on "less"
