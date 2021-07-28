95f11f1bfd537d657b76221ca0b1a3c8a99f823e
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-28 11:07:41 +0200

    Brew formula update for circumflex version 1.17

--- circumflex.rb
+++ circumflex.rb
@@ -5,29 +5,29 @@
 class Circumflex < Formula
   desc "circumflex is Hacker News in your terminal"
   homepage "https://github.com/bensadeh/circumflex/"
-  version "1.16"
+  version "1.17"
   license "AGPL-3.0-only"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bensadeh/circumflex/releases/download/1.16/circumflex_1.16_macOS_64-bit.tar.gz"
-      sha256 "80d6a9d9fa271812695f2453e270f0c95a17a993d3da5e857b3ef6c909657e2d"
+      url "https://github.com/bensadeh/circumflex/releases/download/1.17/circumflex_1.17_macOS_64-bit.tar.gz"
+      sha256 "0240286addc2d12461ef6348d4e4440e94ad6ad8211a9cb4d2b22d39bd98794b"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bensadeh/circumflex/releases/download/1.16/circumflex_1.16_macOS_arm64.tar.gz"
-      sha256 "6d2d35eb76c0713ba685885d677a9d8ec5d480dcb335fa6fbac452fb7153d3b9"
+      url "https://github.com/bensadeh/circumflex/releases/download/1.17/circumflex_1.17_macOS_arm64.tar.gz"
+      sha256 "401ca75f1c55302f6e3125ce4925d8d4a6ea8d02d34ce470a87651c63599c577"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bensadeh/circumflex/releases/download/1.16/circumflex_1.16_Linux_64-bit.tar.gz"
-      sha256 "77b951b018095b94ac2075ab36dcc5d05e67657f5195906f105398e3ca02e7e6"
+      url "https://github.com/bensadeh/circumflex/releases/download/1.17/circumflex_1.17_Linux_64-bit.tar.gz"
+      sha256 "d2b399db3fc5984a4aa461859b82b79597edea82977f51b839306ad3a43fb094"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/bensadeh/circumflex/releases/download/1.16/circumflex_1.16_Linux_arm64.tar.gz"
-      sha256 "150fa3d2c4892d52b50e729089e80ef0522add7c82a0c0413aaacb29e110d1ac"
+      url "https://github.com/bensadeh/circumflex/releases/download/1.17/circumflex_1.17_Linux_arm64.tar.gz"
+      sha256 "6e0dd7303a9e2b355aa923b528ad9645d0cd749fa2f67cece34ae5283cc5dfdf"
     end
   end
 
